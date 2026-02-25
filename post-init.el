4d5ed2e19d3ed3d3b6ffec29221f6191f49b56dd
Wu David <davidwuchn@gmail.com>
2026-02-25 20:49:21 +0800

    ⚒ Enable Emacs config modules (init-system, init-completion, init-evil, init-dev, init-tools)

--- post-init.el
+++ post-init.el
@@ -5,9 +5,8 @@
 (add-to-list 'load-path (expand-file-name "lisp" minimal-emacs-user-directory))
 
 ;; Load the modular configuration files
-;; (require 'theme-setting)
-;; (require 'init-system)
-;; (require 'init-completion)
-;; (require 'init-evil)
-;; (require 'init-dev)
-;; (require 'init-tools)
+(require 'init-system)
+(require 'init-completion)
+(require 'init-evil)
+(require 'init-dev)
+(require 'init-tools)
