e85383951a4f0a5db5931aeb7ec8f8c7c0a909ff
Wu David <davidwuchn@gmail.com>
2026-02-26 01:35:15 +0800

    ◈ Save current system state, plans, and learnings

---
+++ custom.el
@@ -0,0 +1,19 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(apheleia avy cape cider compile-angel corfu dirvish doom-modeline dumb-jump
+              eat eca embark-consult enhanced-evil-paredit evil-collection
+              evil-surround evil-visualstar exec-path-from-shell gptel-agent
+              helpful magit marginalia markdown-mode nerd-icons-completion
+              nerd-icons-corfu nerd-icons-dired tempel treesit-auto undo-fu
+              undo-fu-session vertico vundo)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
