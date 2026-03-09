5e5cdd6a5269e9bddb6d844a985c3ba287b29bb7
Wu David <davidwuchn@gmail.com>
2026-03-09 22:30:09 +0800

    update

--- custom.el
+++ custom.el
@@ -4,13 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(apheleia avy buttercup cape cider clojure-ts-mode compile-angel corfu-terminal
-              dirvish doom-modeline dumb-jump eat eca embark-consult
-              enhanced-evil-paredit evil-collection evil-surround
-              evil-visualstar exec-path-from-shell gptel-agent helpful magit
-              marginalia nerd-icons-completion nerd-icons-corfu nerd-icons-dired
-              tempel treesit-auto undo-fu undo-fu-session vertico vundo)))
+ '(package-selected-packages '(eca)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
