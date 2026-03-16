a6032d58cbca548d4e9be59285cb9c3645765abf
Wu David <davidwuchn@gmail.com>
2026-03-16 19:31:18 +0800

    Δ Add .elc cleanup guard to prevent stale bytecode errors

--- pre-early-init.el
+++ pre-early-init.el
@@ -7,3 +7,13 @@
 ;; NOTE: This must be placed in 'pre-early-init.el'.
 (setq user-emacs-directory (expand-file-name "var/" minimal-emacs-user-directory))
 (setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+
+;; Purge .elc files in init directories to prevent stale bytecode.
+;; These cause insidious void-variable errors when source changes.
+;; (Package .elc files in var/elpa/ are managed by package.el.)
+(dolist (dir (list minimal-emacs-user-directory
+                   (expand-file-name "lisp" minimal-emacs-user-directory)
+                   (expand-file-name "lisp/modules" minimal-emacs-user-directory)))
+  (when (file-directory-p dir)
+    (dolist (elc (directory-files dir t "\\.elc\\'" t))
+      (delete-file elc))))
