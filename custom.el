06890e4765cfa05481ec84114efb6b7d7b607626
Wu David <davidwuchn@gmail.com>
2026-03-19 00:50:15 +0800

    Δ Update custom.el: use safe-local-variable-directories

--- custom.el
+++ custom.el
@@ -4,9 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((eval add-hook 'before-save-hook #'copyright-update nil t) (flymake-mode . t)
-     (emacs-lisp-docstring-fill-column . fill-column))))
+ '(safe-local-variable-directories '("/Users/davidwu/.emacs.d/")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
