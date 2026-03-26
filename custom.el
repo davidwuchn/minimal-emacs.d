4c0c23b74eecac7eb1318b150d569e1a2a23a93b
Wu David <davidwuchn@gmail.com>
2026-03-26 23:17:10 +0800

    ✓ Save subagent timeout setting (300s)

--- custom.el
+++ custom.el
@@ -4,17 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(apheleia auto-package-update avy buffer-terminator buttercup cape cider
-              clojure-ts-mode compile-angel corfu-terminal diff-hl dirvish
-              doom-modeline dumb-jump easysession eat eca embark-consult
-              enhanced-evil-paredit evil-collection evil-surround
-              evil-visualstar exec-path-from-shell goto-chg gptel helpful
-              kirigami magit marginalia nerd-icons-completion nerd-icons-corfu
-              nerd-icons-dired orderless org-appear org-bullets org-download
-              org-modern org-pomodoro org-ql org-roam org-transclusion ox-gfm
-              ox-pandoc ox-reveal tempel treesit-auto treesit-fold undo-fu
-              undo-fu-session vertico vim-tab-bar vterm vundo yaml)))
+ '(my/gptel-agent-task-timeout 300))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
