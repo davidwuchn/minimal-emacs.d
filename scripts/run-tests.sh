202f31cae158c9afd1e81e5f6ec4d7f4158580d6
Wu David <davidwuchn@gmail.com>
2026-04-06 17:41:44 +0800

    ◈ fix: use subr-primitive-p guard and system-name function form

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -41,6 +41,10 @@ run_unit_tests() {
         -L "$DIR/tests" \
         -l ert \
         --eval "(advice-add (quote startup-redirect-eln-cache) :override (lambda (dir) (push (expand-file-name (file-name-as-directory dir) user-emacs-directory) native-comp-eln-load-path)))" \
+<<<<<<< HEAD
+=======
+        --eval "(mapc (lambda (fn) (and (fboundp fn) (subr-primitive-p (symbol-function fn)) (comp-subr-trampoline-install fn))) (quote (file-exists-p call-process kill-buffer message directory-files require featurep process-list process-name system-name)))" \
+>>>>>>> 67bbfd7d (◈ fix: use subr-primitive-p guard and system-name function form)
         $(find tests -name "test-*.el" -exec echo "-l {}" \;) \
         --eval "(ert-run-tests-batch-and-exit \"$PATTERN\")" 2>&1) || true
     
