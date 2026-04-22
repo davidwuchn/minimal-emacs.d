a57648568c618027a570d34c72b3e103bb9d63e5
neopi5 <davidwuchn@me.com>
2026-04-22 19:14:19 +0800

    Merge optimize/agent-neopi5-r190153z8973-exp1 for verification

--- lisp/modules/gptel-tools-agent.el
+++ lisp/modules/gptel-tools-agent.el
@@ -2016,7 +2016,9 @@ subagent callback fired, and avoids reusing a deleted worktree as
                       (void-variable nil)))
                user-emacs-directory
                temporary-file-directory)
-              (and (stringp default-directory) default-directory)
+              (and (stringp default-directory)
+                   (file-directory-p default-directory)
+                   default-directory)
               temporary-file-directory)))
     (with-current-buffer safe-buffer
       (setq default-directory safe-default-directory)
