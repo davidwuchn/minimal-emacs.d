a7a0530d1bae310e236b3ce40d47b40ff5705f76
onepi5 <davidwuchn@me.com>
2026-04-23 12:32:41 +0800

    Merge optimize/cache-onepi5-r110108z9fc2-exp1 for verification

--- lisp/modules/gptel-ext-context-cache.el
+++ lisp/modules/gptel-ext-context-cache.el
@@ -481,7 +481,8 @@ Image tokens are counted from `gptel-context' if available."
           (setq my/gptel--context-window-cache-data nil
                 my/gptel--context-window-cache-last-refresh nil)
           (load my/gptel-context-window-cache-file nil t)
-          (when (listp my/gptel--context-window-cache-data)
+          (when (and (listp my/gptel--context-window-cache-data)
+                     (hash-table-p my/gptel--context-window-cache))
             (dolist (kv my/gptel--context-window-cache-data)
               (let ((key (car kv)) (val (cdr kv)))
                 (cond
