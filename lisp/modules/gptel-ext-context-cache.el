0be04b8c06955a81fc8a65bc7abf886749e20a13
riven <davidwuchn@me.com>
2026-05-03 06:33:45 +0800

    Merge optimize/cache-riven-r030002z236e-exp1 for verification

--- lisp/modules/gptel-ext-context-cache.el
+++ lisp/modules/gptel-ext-context-cache.el
@@ -864,7 +864,9 @@ Note: OpenRouter fetch is NOT triggered here - use `my/gptel-refresh-context-win
         (my/gptel--cache-context-window model-id cw)))
      ((let* ((meta (my/gptel-get-model-metadata model-id))
              (cw (plist-get meta :context-window)))
-        (my/gptel--cache-context-window model-id cw)))
+        (when (my/gptel--positive-integer-p cw)
+          (my/gptel--cache-context-window model-id cw))
+        cw))
      (t my/gptel-default-context-window))))
 
 ;;; Auto-refresh Timer
