a2d0f7a7e7ac29424803c46d3539cbd064462d29
onepi5 <davidwuchn@me.com>
2026-05-11 09:49:31 +0800

    + add rainbow-delimiters, nerd-icons-ibuffer, gcmh

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -65,4 +65,24 @@
   :ensure t
   :hook (after-init . dtrt-indent-global-mode))
 
+;; rainbow-delimiters: Color-coded parentheses by nesting depth
+;; Source: https://github.com/Fanael/rainbow-delimiters
+(use-package rainbow-delimiters
+  :ensure t
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; nerd-icons-ibuffer: File icons in buffer list
+;; Source: https://github.com/seagle0128/nerd-icons-ibuffer
+(use-package nerd-icons-ibuffer
+  :ensure t
+  :hook (ibuffer-mode . nerd-icons-ibuffer-mode))
+
+;; gcmh: Garbage collector optimization — reduces GC pauses during heavy operations
+;; Source: https://gitlab.com/koral/gcmh
+(use-package gcmh
+  :ensure t
+  :hook (after-init . gcmh-mode)
+  :custom
+  (gcmh-high-cons-threshold (* 64 1024 1024)))
+
 ;;; init-editor.el ends here
