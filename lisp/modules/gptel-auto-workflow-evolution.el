f7b0a9b3127a9d29148a0c23d633c8b741792ee4
imacpro <davidwuchn@gmail.com>
2026-05-22 18:10:10 +0800

    ⚒ Soft-require research-benchmark: fix void-function load-research-traces

--- lisp/modules/gptel-auto-workflow-evolution.el
+++ lisp/modules/gptel-auto-workflow-evolution.el
@@ -17,6 +17,8 @@
 
 ;; Soft require: research-integration may not exist on all deployments
 (condition-case nil (require 'gptel-auto-workflow-research-integration nil t) (ignore))
+;; Soft require: research-benchmark provides load-research-traces
+(condition-case nil (require 'gptel-auto-workflow-research-benchmark nil t) (ignore))
 
 ;; External functions from other modules
 (declare-function gptel-auto-workflow--worktree-base-root "gptel-tools-agent" ())
